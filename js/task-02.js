4f3fc6339188f8c76704782f239da85a211240df
RitaDyvnych <m.dyvnych@gmail.com>
2021-07-14 16:38:08 +0300

    написаны задания 1 и 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const makeIngredientListEl = (ingredients) => {
+  return ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+    liEl.textContent = ingredient;
+    return liEl;
+  })
+}
+
+const elements = makeIngredientListEl(ingredients);
+const liElement = document.querySelector('ul#ingredients');
+liElement.append(...elements);
